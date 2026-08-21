381131844ceda18bbe43dc6688a730689859be32
saltyfishthegreat <158099593+saltyfishthegreat@users.noreply.github.com>
2026-08-21 23:35:06 +0800

    Update task.tsx

--- src/component/task.tsx
+++ src/component/task.tsx
@@ -1,9 +1,9 @@
 export default function task(){
     return (
-        <div className = "flex flex-row items-center justify-center w-4/5 h-1/6 gap-2 ">
+        <div className = "flex flex-row items-center justify-center w-4/5 h-1/6 gap-2 border-2 border-gray-300 rounded-lg shadow-md p-2">
             <img></img>
             <a>吃饭</a>
-            <div className = "flex flex-row items-center justify-center gap-2 border-2 border-gray-300 rounded-lg shadow-md p-2">
+            <div className = "flex flex-row items-center justify-center h-7/8 gap-2 border-2 border-gray-300 rounded-lg shadow-md p-2">
                 <input type='checkbox'></input>
                 <a>in progress</a>
             </div>
